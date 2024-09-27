4fab59275e0ce8a27661e100f082f2c98dc0b7eb
mannymorena <mannymorena@users.noreply.github.com>
2024-09-26 22:13:30 -0400

    fixes

--- main.tf
+++ main.tf
@@ -58,27 +58,27 @@ module "alb" {
     bucket = "my-alb-logs"
   }
 
-  listeners = {
-    ex-http-https-redirect = {
+  http_tcp_listeners = [
+    {
       port     = 80
       protocol = "HTTP"
-      redirect = {
-        port        = "443"
-        protocol    = "HTTPS"
-        status_code = "HTTP_301"
-      }
+      target_group_index = 0
     }
   }
 
-  target_groups = {
-    ex-instance = {
+  target_groups = [
+    {
       name_prefix      = "blog-"
-      protocol         = "HTTP"
-      port             = 80
+      backend_protocol = "HTTP"
+      backend_port     = 80
       target_type      = "instance"
-      target_id        = aws_instance.blog.id
+      targets = {
+        target_id      = aws_instance.blog.id
+        port = 80
+      }
     }
   }
+  ]
 
   tags = {
     Environment = "dev"
